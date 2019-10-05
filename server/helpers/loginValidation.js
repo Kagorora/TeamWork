ff6b7b7f086d1aa862843518f811472dd8f5b3c1
kagorora <kagororamaxime@gmail.com>
2019-10-05 20:14:37 +0300

    chore(gh-pages): remove backend from server
     - remove server folder into gh-pages
    
    [Finishes: #168968212]

--- server/helpers/loginValidation.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const loginSchema = Joi.object().keys({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(3).required(),
-});
-export default loginSchema;
